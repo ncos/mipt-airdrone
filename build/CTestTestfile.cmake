da71be6450ef0e0447841cc3a4166e12a599bf84
ncos <anton.mitrokhin@phystech.edu>
2014-03-11 10:13:56 +0400

    [master] The project start

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/ncos/catkin_ws/src
+# Build directory: /home/ncos/catkin_ws/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(airdrone)
+SUBDIRS(laserscan_server)
+SUBDIRS(rgbd_shrinker)
+SUBDIRS(velocity_server)
