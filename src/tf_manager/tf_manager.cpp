9042aa65235886376cf3ff7b3dd58152ee278159
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2014-09-11 08:42:43 +0400

    tf to mesage in tf_manager

--- src/tf_manager/tf_manager.cpp
+++ src/tf_manager/tf_manager.cpp
@@ -1,31 +1,19 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
-
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 class TfHandle
 {
 private:
-    tf::TransformListener tf_listener;
+    ros::Subscriber sub_ekf;
     tf::TransformBroadcaster tf_broadcaster;
 
-    void split_pose_ekf()
+    void split_pose_ekf(const geometry_msgs::PoseWithCovarianceStamped& msg_in)
     {
-        tf::StampedTransform transform_in;
-
-        try {
-            this->tf_listener.lookupTransform("/odom", "/ekf_result_frame", ros::Time(0), transform_in);
-        }
-        catch (tf::TransformException &ex) {
-            ROS_ERROR("%s",ex.what());
-            ros::Duration(1.0).sleep();
-            return;
-        }
-
-
         double roll, pitch, yaw;
-        tf::Matrix3x3(transform_in.getRotation()).getRPY(roll, pitch, yaw);
-
+        tf::Quaternion orient(msg_in.pose.pose.orientation.x, msg_in.pose.pose.orientation.y, msg_in.pose.pose.orientation.z, msg_in.pose.pose.orientation.w);
+        tf::Matrix3x3(orient).getRPY(roll, pitch, yaw);
 
         // broadcast
         // Examine Hector slam tutorial/How to set up hector_slam for your robot
@@ -33,40 +21,36 @@ private:
         // the link may change with time
         // base_footprint
         tf::Transform transform_odom2base_footprint;
-        transform_odom2base_footprint.setOrigin( tf::Vector3(transform_in.getOrigin().x(),
-                                                             transform_in.getOrigin().y(),
+        transform_odom2base_footprint.setOrigin( tf::Vector3(msg_in.pose.pose.position.x,
+                                                             msg_in.pose.pose.position.y,
                                                              0.0) );
 
         tf::Quaternion q = tf::createQuaternionFromRPY(0, 0, yaw);
         transform_odom2base_footprint.setRotation(q);
-        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_odom2base_footprint, transform_in.stamp_, "odom", "base_footprint"));
+        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_odom2base_footprint, msg_in.header.stamp, "odom", "base_footprint"));
 
         // base_stabilized
         tf::Transform transform_base_footprint2base_stabilized;
-        transform_base_footprint2base_stabilized.setOrigin( tf::Vector3(0.0, 0.0, transform_in.getOrigin().z()) );
+        transform_base_footprint2base_stabilized.setOrigin( tf::Vector3(0.0, 0.0, msg_in.pose.pose.position.z) );
 
         transform_base_footprint2base_stabilized.setRotation(tf::createQuaternionFromRPY(0, 0, 0));
-        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_base_footprint2base_stabilized, transform_in.stamp_,
+        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_base_footprint2base_stabilized, msg_in.header.stamp,
                                                                                                          "base_footprint", "base_stabilized"));
 
         // base_link
         tf::Transform transform_base_stabilized2base_link;
         transform_base_stabilized2base_link.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
         transform_base_stabilized2base_link.setRotation(tf::createQuaternionFromRPY(roll, pitch, 0));
-        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_base_stabilized2base_link, transform_in.stamp_,
+        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_base_stabilized2base_link, msg_in.header.stamp,
                                                                                                               "base_stabilized", "base_link"));
     }
 
 public:
-    TfHandle()
+    TfHandle(ros::NodeHandle nh)
     {
-        this->tf_listener.waitForTransform("/odom", "/ekf_result_frame", ros::Time::now(), ros::Duration(3.0));
+        this->sub_ekf = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped> ("robot_pose_ekf/odom_combined", 1, this->split_pose_ekf);
     }
 
-    void spin()
-    {
-        this->split_pose_ekf();
-    }
 };
 
 
@@ -76,13 +60,9 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "airdrine_tf_manager");
     ros::NodeHandle node;
     ros::Rate rate(30.0);
-    TfHandle tf_handle;
+    TfHandle tf_handle(node);
 
-
-    while (node.ok()) {
-        tf_handle.spin();
-        rate.sleep();
-    }
+    ros::spin();
     
     return 0;
 };
