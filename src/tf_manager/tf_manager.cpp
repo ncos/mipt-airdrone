7a40412fa92cc38693b1019e3bb243a017decb55
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2014-09-12 08:26:09 +0400

    changed tf_manager input from tf to topic, allowing to remove custom robot_pose_ekf package

--- src/tf_manager/tf_manager.cpp
+++ src/tf_manager/tf_manager.cpp
@@ -9,10 +9,10 @@ private:
     ros::Subscriber sub_ekf;
     tf::TransformBroadcaster tf_broadcaster;
 
-    void split_pose_ekf(const geometry_msgs::PoseWithCovarianceStamped& msg_in)
+    void split_pose_ekf(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg_in)
     {
         double roll, pitch, yaw;
-        tf::Quaternion orient(msg_in.pose.pose.orientation.x, msg_in.pose.pose.orientation.y, msg_in.pose.pose.orientation.z, msg_in.pose.pose.orientation.w);
+        tf::Quaternion orient(msg_in->pose.pose.orientation.x, msg_in->pose.pose.orientation.y, msg_in->pose.pose.orientation.z, msg_in->pose.pose.orientation.w);
         tf::Matrix3x3(orient).getRPY(roll, pitch, yaw);
 
         // broadcast
@@ -21,34 +21,34 @@ private:
         // the link may change with time
         // base_footprint
         tf::Transform transform_odom2base_footprint;
-        transform_odom2base_footprint.setOrigin( tf::Vector3(msg_in.pose.pose.position.x,
-                                                             msg_in.pose.pose.position.y,
+        transform_odom2base_footprint.setOrigin( tf::Vector3(msg_in->pose.pose.position.x,
+                                                             msg_in->pose.pose.position.y,
                                                              0.0) );
 
         tf::Quaternion q = tf::createQuaternionFromRPY(0, 0, yaw);
         transform_odom2base_footprint.setRotation(q);
-        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_odom2base_footprint, msg_in.header.stamp, "odom", "base_footprint"));
+        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_odom2base_footprint, msg_in->header.stamp, "odom", "base_footprint"));
 
         // base_stabilized
         tf::Transform transform_base_footprint2base_stabilized;
-        transform_base_footprint2base_stabilized.setOrigin( tf::Vector3(0.0, 0.0, msg_in.pose.pose.position.z) );
+        transform_base_footprint2base_stabilized.setOrigin( tf::Vector3(0.0, 0.0, msg_in->pose.pose.position.z) );
 
         transform_base_footprint2base_stabilized.setRotation(tf::createQuaternionFromRPY(0, 0, 0));
-        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_base_footprint2base_stabilized, msg_in.header.stamp,
+        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_base_footprint2base_stabilized, msg_in->header.stamp,
                                                                                                          "base_footprint", "base_stabilized"));
 
         // base_link
         tf::Transform transform_base_stabilized2base_link;
         transform_base_stabilized2base_link.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
         transform_base_stabilized2base_link.setRotation(tf::createQuaternionFromRPY(roll, pitch, 0));
-        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_base_stabilized2base_link, msg_in.header.stamp,
+        this->tf_broadcaster.sendTransform(tf::StampedTransform(transform_base_stabilized2base_link, msg_in->header.stamp,
                                                                                                               "base_stabilized", "base_link"));
     }
 
 public:
     TfHandle(ros::NodeHandle nh)
     {
-        this->sub_ekf = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped> ("robot_pose_ekf/odom_combined", 1, this->split_pose_ekf);
+        this->sub_ekf = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped> ("robot_pose_ekf/odom", 1, &TfHandle::split_pose_ekf, this);
     }
 
 };
