e6ecce74e3cdb213dcc25d5bfbb5be296d005060
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2014-09-12 23:50:04 +0400

    reduced rgbd_shrimker

--- src/sensor_drivers/rgbd_shrinker/main.cpp
+++ src/sensor_drivers/rgbd_shrinker/main.cpp
@@ -9,9 +9,7 @@
 #include "pcl_ros/point_cloud.h"
 #include <pcl/point_types.h>
 #include <pcl/common/common_headers.h>
-#include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/filters/passthrough.h>
-#include <pcl/filters/voxel_grid.h>
 
 
 using std::cout;
@@ -20,12 +18,8 @@ using std::endl;
 
 ros::Subscriber sub;
 ros::Publisher  pub_laser;
-ros::Publisher  pub_floor;
-
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr laser_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PointCloud<pcl::PointXYZ>::Ptr floor_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-
 
 
 struct Cmp_class {
@@ -37,30 +31,10 @@ struct Cmp_class {
 } cmp_class;
 
 
-
-
-
-
-
 //ros::Time start;
-void floor_filter(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud)
-{
-	pcl::PassThrough<pcl::PointXYZ> pass;
-	pass.setInputCloud (cloud);
-	pass.setFilterFieldName ("y");    // z is to front, y is DOWN!
-	pass.setFilterLimits (-0.2, 4.0); // (-0.2, 4.0)
-	pass.filter (*floor_cloud);
-
-	// Downsampling the cloud //
-	pcl::VoxelGrid<pcl::PointXYZ> sor;
-	sor.setInputCloud (floor_cloud);
-	sor.setLeafSize (0.05f, 0.05f, 0.05f); // 0.05f is a good value here
-	sor.filter (*floor_cloud);
-};
-
-
 void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud)
 {
+    cerr << "\tIdle_sec: " << cloud->header.stamp << "\n";
 	//cerr << "\tIdle_sec: " << ros::Time::now().toSec() - start.toSec(); start = ros::Time::now();
 	pcl::PassThrough<pcl::PointXYZ> pass;
 	pass.setInputCloud (cloud);
@@ -75,15 +49,8 @@ void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud)
 
 
 	pub_laser.publish(laser_cloud);
-	//cerr << "\tLaser_sec: " << ros::Time::now().toSec() - start.toSec(); start = ros::Time::now();
-	// --------------------
-	floor_filter(cloud);
-	pub_floor.publish(floor_cloud);
-	//cerr << "\tFF_sec: " << ros::Time::now().toSec() - start.toSec() << endl; start = ros::Time::now();
+	//cerr << "\tLaser_sec: " << ros::Time::now().toSec() - start.toSec() << "\n"; start = ros::Time::now();
 };
-
-
-
 
 
 
@@ -94,15 +61,11 @@ int main (int argc, char** argv)
 	ros::NodeHandle nh;
 	std::string input_topic        = nh.resolveName("/sensors/kinect/depth/points" );
 	std::string output_laser_topic = nh.resolveName("/shrinker/depth/laser_points");
-	std::string output_floor_topic = nh.resolveName("/shrinker/depth/floor_points");
-
 
 	sub       = nh.subscribe<pcl::PointCloud<pcl::PointXYZ> > (input_topic,  1, callback);
 	pub_laser = nh.advertise<pcl::PointCloud<pcl::PointXYZ> > (output_laser_topic, 1);
-	pub_floor = nh.advertise<pcl::PointCloud<pcl::PointXYZ> > (output_floor_topic, 1);
 
 	ros::spin ();
-
 	return 0;
 }
 
