899e9a76e8416ca5ca640365ee3ccf3f749ccd45
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2014-10-05 20:16:14 +0400

    velocity controller

--- src/vel_cntrl/main.cpp
+++ src/vel_cntrl/main.cpp
@@ -22,35 +22,45 @@ std::string input_vel_kbrd_topic;
 std::string output_vel_topic;
 std::string output_rc_topic;
 std::string visualization_topic;
+std::string fixed_frame;
 std::string input_velocity_frame;
 std::string output_velocity_frame;
 
+
+double roll_min, roll_max;
+double pitch_min, pitch_max;
+double yaw_min, yaw_max;
+double throttle_min, throttle_max;
 double vel_to_pwr;
 bool in_simulator;
 bool keyboard_control;
 
-
 geometry_msgs::Twist vel_cntrl_;
 geometry_msgs::Twist vel_kbrd_;
-double rc_acc[8];
-vel_cntrl::RC rc_msg;
 
 #define ROLL 0
 #define PITCH 1
 #define YAW 3
 #define THROTTLE 2
 
-
 class TransformManager
 {
 private:
     ros::NodeHandle nh;
     tf::TransformListener tf_listener;
+    tf::StampedTransform last_known_tf;
 
 public:
-    TransformManager(ros::NodeHandle nh_) {
+    geometry_msgs::Twist current_vel;
+
+    TransformManager(ros::NodeHandle nh_)
+    {
         this->nh = nh_;
-        try { this->tf_listener.waitForTransform(input_velocity_frame, output_velocity_frame, ros::Time(0), ros::Duration(10.0) ); }
+        last_known_tf.setIdentity();
+        try {
+            this->tf_listener.waitForTransform(input_velocity_frame, output_velocity_frame, ros::Time(0), ros::Duration(10.0) );
+            this->tf_listener.waitForTransform(fixed_frame, output_velocity_frame, ros::Time(0), ros::Duration(10.0) );
+        }
         catch (tf::TransformException &ex) { ROS_ERROR("[velocity_server]: (wait) %s", ex.what()); }
     }
 
@@ -83,53 +93,127 @@ public:
         rotated.z = vector_transformed_end.point.z - vector_transformed_start.point.z;
         return rotated;
     }
+
+    void current_vel_renew ()
+    {
+        tf::StampedTransform transform;
+        try { this->tf_listener.lookupTransform(fixed_frame, output_velocity_frame, ros::Time(0), transform); }
+        catch (tf::TransformException &ex) { ROS_ERROR("[velocity_server]: (lookup) Unable to transform: %s", ex.what()); }
+        double dt = (transform.stamp_.toSec() - this->last_known_tf.stamp_.toSec());
+        if (dt < 0.1) return;
+        tf::Transform past_to_current = this->last_known_tf.inverseTimes(transform);
+        double droll, dpitch, dyaw;
+        tf::Matrix3x3(past_to_current.getRotation()).getRPY(droll, dpitch, dyaw);
+        this->last_known_tf = transform;
+        this->current_vel.linear.x = past_to_current.getOrigin().x() / dt;
+        this->current_vel.linear.y = past_to_current.getOrigin().y() / dt;
+        this->current_vel.linear.z = past_to_current.getOrigin().z() / dt;
+        this->current_vel.angular.x = droll  / dt;
+        this->current_vel.angular.y = dpitch / dt;
+        this->current_vel.angular.z = dyaw   / dt;
+    }
 };
 
 boost::shared_ptr<TransformManager> transform_manager;
 
 
+class VelocityToPower
+{
+private:
+    double rc_cmd[8];
+    double rc_cmd_max[8];
+    double rc_cmd_min[8];
+    double rc_cmd_zero[8];
+
+public:
+    VelocityToPower()
+    {
+        rc_cmd_max [ROLL]     = roll_max;
+        rc_cmd_min [ROLL]     = roll_min;
+        rc_cmd_zero[ROLL]     = (rc_cmd_min [ROLL] + rc_cmd_max [ROLL]) / 2.0;
+        rc_cmd_max[PITCH]     = pitch_max;
+        rc_cmd_min[PITCH]     = pitch_min;
+        rc_cmd_zero[PITCH]    = (rc_cmd_min [PITCH] + rc_cmd_max [PITCH]) / 2.0;
+        rc_cmd_max[YAW]       = yaw_max;
+        rc_cmd_min[YAW]       = yaw_min;
+        rc_cmd_zero[YAW]      = (rc_cmd_min [YAW] + rc_cmd_max [YAW]) / 2.0;
+        rc_cmd_max[THROTTLE]  = throttle_max;
+        rc_cmd_min[THROTTLE]  = throttle_min;
+        rc_cmd_zero[THROTTLE] = rc_cmd_min[THROTTLE];
+
+        for (int i = 0; i < 8; ++i) {
+            rc_cmd[i] = rc_cmd_zero[i];
+        }
+    }
+
+    vel_cntrl::RC manual_control()
+    {
+        vel_cntrl::RC rc_msg;
+        for(int i = 0; i < 8; ++i) rc_msg.channel.elems[i] = 0;
+        return rc_msg;
+    }
+
+
+    vel_cntrl::RC all_neutral()
+    {
+        vel_cntrl::RC rc_msg;
+        for(int i = 0; i < 8; ++i) rc_msg.channel.elems[i] = int(rc_cmd_zero[i]);
+        return rc_msg;
+    }
+
+
+    vel_cntrl::RC from_vel(geometry_msgs::Twist vel)
+    {
+        vel_cntrl::RC rc_msg;
+        geometry_msgs::Twist current_vel = get_current_vel();
+
+        rc_cmd[ROLL]     += vel_to_pwr * (vel.linear.y  - current_vel.linear.y);
+        rc_cmd[PITCH]    += vel_to_pwr * (vel.linear.x  - current_vel.linear.x);
+        rc_cmd[YAW]      += vel_to_pwr * (vel.angular.z - current_vel.angular.z);
+        rc_cmd[THROTTLE] += vel_to_pwr * (vel.linear.z  - current_vel.linear.z);
+
+        for(int i = 0; i < 8; ++i) {
+            if (rc_cmd[i] > rc_cmd_max[i]) rc_cmd[i] = rc_cmd_max[i];
+            if (rc_cmd[i] < rc_cmd_min[i]) rc_cmd[i] = rc_cmd_min[i];
+        }
+
+        for(int i = 0; i < 8; ++i)
+            rc_msg.channel.elems[i] = int(rc_cmd[i]);
+        return rc_msg;
+    }
+
+    geometry_msgs::Twist get_current_vel ()
+    {
+        return transform_manager->current_vel;
+    }
+};
+
 
 void callback_cntrl(const geometry_msgs::Twist vel)
 {
-    vel_cntrl_.linear = transform_manager->kin_to_base(vel.linear);
-    vel_cntrl_.angular.x = vel.angular.x;
-    vel_cntrl_.angular.y = vel.angular.y;
-    vel_cntrl_.angular.z = vel.angular.z;
+    if (keyboard_control == false) {
+        transform_manager->current_vel_renew();
+        vel_cntrl_.linear = transform_manager->kin_to_base(vel.linear);
+        vel_cntrl_.angular.x = vel.angular.x;
+        vel_cntrl_.angular.y = vel.angular.y;
+        vel_cntrl_.angular.z = vel.angular.z;
+    }
 };
 
 void callback_kbrd(const geometry_msgs::Twist vel)
 {
-    vel_kbrd_.linear = transform_manager->kin_to_base(vel.linear);
-    vel_kbrd_.angular.x = vel.angular.x;
-    vel_kbrd_.angular.y = vel.angular.y;
-    vel_kbrd_.angular.z = vel.angular.z;
+    if (keyboard_control == true) {
+        transform_manager->current_vel_renew();
+        vel_kbrd_.linear = transform_manager->kin_to_base(vel.linear);
+        vel_kbrd_.angular.x = vel.angular.x;
+        vel_kbrd_.angular.y = vel.angular.y;
+        vel_kbrd_.angular.z = vel.angular.z;
+    }
 };
 
 
-void set_manual_control()
-{
-    for(int i = 0; i < 8; i++) rc_msg.channel.elems[i] = 0;
-};
 
-
-void set_all_min()
-{
-    rc_msg.channel.elems[THROTTLE] = 1000;
-};
-
-
-void vel_to_RC(geometry_msgs::Twist vel)
-{
-    rc_acc[THROTTLE] += vel_to_pwr * vel.linear.z;
-    if(rc_acc[THROTTLE] > 2000) rc_acc[THROTTLE] = 2000;
-    if(rc_acc[THROTTLE] < 1000) rc_acc[THROTTLE] = 1000;
-
-    for(int i = 0; i < 8; i++)
-        rc_msg.channel.elems[i] = rc_acc[i];
-};
-
-
-void state_to_rviz(bool inf = false)
+void state_to_rviz()
 {
     visualization_msgs::Marker height_text;
     height_text.header.frame_id = "/kinect_link";
@@ -145,15 +229,39 @@ void state_to_rviz(bool inf = false)
     height_text.color.a = 0.3;
     height_text.lifetime = ros::Duration(1000);
     height_text.text    = "THROTTLE = [Nan]\nROLL = [Nan]\nPITCH = [Nan]\nYAW = [Nan]\n";
-    if (!inf) {
-        char text[128];
-        height_text.lifetime = ros::Duration(0.2);
-        sprintf(text, "THROTTLE = [%4.0f]\nROLL = [%4.0f]\nPITCH = [%4.0f]\nYAW = [%4.0f]\n", rc_acc[THROTTLE], rc_acc[ROLL], rc_acc[PITCH], rc_acc[YAW]);
-        height_text.text = text;
-    }
     pub_mrk.publish(height_text);
 };
 
+
+void state_to_rviz(vel_cntrl::RC rc_msg)
+{
+    visualization_msgs::Marker height_text;
+    height_text.header.frame_id = "/kinect_link";
+    height_text.ns = "text_ns";
+    height_text.action = visualization_msgs::Marker::ADD;
+    height_text.id = 101;
+    height_text.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    height_text.scale.z = 0.1;
+    height_text.pose.position.z = 0.4;
+    height_text.color.r = 0.0;
+    height_text.color.g = 1.0;
+    height_text.color.b = 0.0;
+    height_text.color.a = 0.3;
+    char text[256];
+    height_text.lifetime = ros::Duration(0.2);
+    sprintf(text, "THROTTLE = [%d]\nROLL = [%d]\nPITCH = [%d]\nYAW = [%d]\n\nveloocity:\n\tlinear (%3.3f, %3.3f, %3.3f)\n\tangular (%3.3f, %3.3f, %3.3f)\n",
+                          rc_msg.channel.elems[THROTTLE], rc_msg.channel.elems[ROLL],
+                          rc_msg.channel.elems[PITCH], rc_msg.channel.elems[YAW],
+                          transform_manager->current_vel.linear.x,
+                          transform_manager->current_vel.linear.y,
+                          transform_manager->current_vel.linear.z,
+                          transform_manager->current_vel.angular.x,
+                          transform_manager->current_vel.angular.y,
+                          transform_manager->current_vel.angular.z
+    );
+    height_text.text = text;
+    pub_mrk.publish(height_text);
+};
 
 
 
@@ -169,21 +277,29 @@ int main( int argc, char** argv )
     if (!nh.getParam("velocity_server/output_rc_topic", output_rc_topic)) output_rc_topic = "/apm/send_rc";
     if (!nh.getParam("velocity_server/visualization_topic", visualization_topic)) visualization_topic = "/velocity_server/markers";
     if (!nh.getParam("velocity_server/vel_to_pwr", vel_to_pwr)) vel_to_pwr = 0.0;
+    if (!nh.getParam("velocity_server/fixed_frame", fixed_frame)) fixed_frame = "/odom";
     if (!nh.getParam("velocity_server/input_velocity_frame",  input_velocity_frame)) input_velocity_frame = "/kinect_link";
     if (!nh.getParam("velocity_server/output_velocity_frame", output_velocity_frame)) output_velocity_frame  = "/base_link";
+    if (!nh.getParam("velocity_server/roll_min", roll_min)) roll_min  = 1000.0;
+    if (!nh.getParam("velocity_server/roll_max", roll_max)) roll_max  = 2000.0;
+    if (!nh.getParam("velocity_server/pitch_min", pitch_min)) pitch_min  = 1000.0;
+    if (!nh.getParam("velocity_server/pitch_max", pitch_max)) pitch_max  = 2000.0;
+    if (!nh.getParam("velocity_server/yaw_min", yaw_min)) yaw_min  = 1000.0;
+    if (!nh.getParam("velocity_server/yaw_max", yaw_max)) yaw_max  = 2000.0;
+    if (!nh.getParam("velocity_server/throttle_min", throttle_min)) throttle_min  = 1000.0;
+    if (!nh.getParam("velocity_server/throttle_max", throttle_max)) throttle_max  = 2000.0;
     if (!nh.getParam("/keyboard_control", keyboard_control)) keyboard_control = false;
 
-
     transform_manager = boost::shared_ptr<TransformManager> (new TransformManager(nh));
+    VelocityToPower vel_to_rc;
 
-
-    sub_vel_cntrl = nh.subscribe<geometry_msgs::Twist > (input_vel_cntrl_topic, 1, callback_cntrl);
-    sub_vel_kbrd  = nh.subscribe<geometry_msgs::Twist > (input_vel_kbrd_topic,  1, callback_kbrd);
+    sub_vel_cntrl = nh.subscribe<geometry_msgs::Twist > (input_vel_cntrl_topic,  1, callback_cntrl);
+    sub_vel_kbrd  = nh.subscribe<geometry_msgs::Twist > (input_vel_kbrd_topic,   1, callback_kbrd);
     pub_vel     = nh.advertise<geometry_msgs::Twist >      (output_vel_topic,    1 );
     pub_rc      = nh.advertise<vel_cntrl::RC        >      (output_rc_topic,     1 );
     pub_mrk     = nh.advertise<visualization_msgs::Marker> (visualization_topic, 5 );
 
-    state_to_rviz(true);
+    state_to_rviz();
 
     if (!nh.getParam("/in_simulator", in_simulator)) {
         ROS_ERROR("[velocity_server]: the 'in_simulator' boolean parameter must be specified!");
@@ -218,12 +334,10 @@ int main( int argc, char** argv )
             output_vel.angular.z = vel_cntrl_.angular.z;
         }
 
-        vel_to_RC(output_vel);
-
-
+        vel_cntrl::RC rc_msg = vel_to_rc.from_vel(output_vel);
         pub_vel.publish(output_vel);
         pub_rc.publish(rc_msg);
-        state_to_rviz();
+        state_to_rviz(rc_msg);
 
         ros::spinOnce();
         loop_rate.sleep();
